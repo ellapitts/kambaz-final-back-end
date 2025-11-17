7d50344ac1c6ad38f4bf2b27b93ca729561184ed
Ella <pitts.ell@northeastern.edu>
2025-11-17 12:32:39 -0500

    Kambaz Database, courses, modules, enrollment, users functionality added

---
+++ Kambaz/Enrollments/dao.js
@@ -0,0 +1,10 @@
+// When a course is created, it needs to be associated with the creator. In a new Enrollments/dao.js file, implement enrollUserInCourse to enroll, or associate, a user to a course.
+
+import { v4 as uuidv4 } from "uuid";
+export default function EnrollmentsDao(db) {
+  function enrollUserInCourse(userId, courseId) {
+    const { enrollments } = db;
+    enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
+  }
+  return { enrollUserInCourse };
+}
